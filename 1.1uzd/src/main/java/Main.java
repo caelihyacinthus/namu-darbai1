dfda434f6c834d1906bee8b0e71374dd8a872398
vejas valiukonis <vejasva@gmail.com>
2025-01-30 13:11:38 +0200

    1.1uzd, 1.2uzd

--- 1.1uzd/src/main/java/Main.java
+++ 1.1uzd/src/main/java/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
-    String text = "";
+    String text = "Yes";
     while (!text.equals("No")) {
       System.out.println("Yes or No");
       text = scan.nextLine();
