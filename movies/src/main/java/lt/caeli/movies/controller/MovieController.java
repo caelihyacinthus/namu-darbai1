41a71b911238b9a3685acdc3755fb7f9777e2282
vejas valiukonis <vejasva@gmail.com>
2025-02-03 20:45:21 +0200

    2.5

--- movies/src/main/java/lt/caeli/movies/controller/MovieController.java
+++ movies/src/main/java/lt/caeli/movies/controller/MovieController.java
@@ -1,5 +1,6 @@
 package lt.caeli.movies.controller;
 
+import com.fasterxml.jackson.databind.type.ResolvedRecursiveType;
 import lt.caeli.movies.model.Movie;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -31,23 +32,55 @@ public class MovieController {
     return ResponseEntity.ok(movie);
   }
 
-  @PostMapping("/movies")
-  public ResponseEntity<?> addMovie(@RequestBody Movie movie) {
-    if (movie.getTitle() == null || movie.getDirector() == null) {
-      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("movie title empty");
-    }
-
-    movies.add(movie);
-
-    return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest()
-            .path("/{index}")
-            .buildAndExpand(movies.size() - 1).toUri()).body(movie);
-  }
-
   @GetMapping("/movies/search")
   public ResponseEntity<List<Movie>> searchByTitle(@RequestParam String title) {
     List<Movie> movieList = movies.stream().filter(m -> m.getTitle().toLowerCase().contains(title.toLowerCase())).toList();
 
     return ResponseEntity.ok(movieList);
   }
+
+  @PostMapping("/movies")
+  public ResponseEntity<?> addMovie(@RequestBody Movie movie) {
+    if (movie.getTitle() == null || movie.getDirector() == null) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("movie title or director empty");
+    }
+
+    movies.add(movie);
+
+    return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest().path("/{index}")
+                    .buildAndExpand(movies.size() - 1)
+                    .toUri())
+            .body(movie);
+  }
+
+  @PutMapping("/movies/{index}")
+  public ResponseEntity<?> updateMovie(@PathVariable int index, @RequestBody Movie movie) {
+    if (movie.getTitle() == null || movie.getTitle().isEmpty()) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("movie title empty");
+    } else if (movie.getDirector() == null || movie.getDirector().isEmpty()) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("director name empty");
+    } else if (index < movies.size()) {
+      movies.get(index).setTitle(movie.getTitle());
+      movies.get(index).setDirector(movie.getDirector());
+      return ResponseEntity.ok(movie);
+    } else {
+      movies.add(movie);
+      return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest().path("/{index}")
+                      .buildAndExpand(movies.size() - 1)
+                      .toUri())
+              .body(movie);
+    }
+  }
+
+  @DeleteMapping("/movies/{index}")
+  public ResponseEntity<?> deleteMovie(@PathVariable int index) {
+    if (index >= movies.size()) {
+      return ResponseEntity.notFound().build();
+    } else {
+      movies.remove(index);
+      return ResponseEntity.noContent().build();
+    }
+  }
+
+
 }
