bd16530414ddafc5a01ab5b5dd4cf5a1b0d62e8f
vejas valiukonis <vejasva@gmail.com>
2025-02-03 14:37:52 +0200

    2.3,2.4

--- movies/src/main/java/lt/caeli/movies/controller/MovieController.java
+++ movies/src/main/java/lt/caeli/movies/controller/MovieController.java
@@ -1,17 +1,19 @@
 package lt.caeli.movies.controller;
 
 import lt.caeli.movies.model.Movie;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
 public class MovieController {
-  private List<Movie> movies = new ArrayList<>(List.of(new Movie("hell", "pupupu"), new Movie("heaven", "oioioi")));
+  private List<Movie> movies = new ArrayList<>(List.of(new Movie("hell", "pupupu"), new Movie("earth", "uiuouo"), new Movie("heaven", "oioioi")));
 
   @GetMapping("/movies")
   public ResponseEntity<List<Movie>> getMovies() {
@@ -21,22 +23,18 @@ public class MovieController {
   @GetMapping("/movies/{index}")
   public ResponseEntity<Movie> getMovieById(@PathVariable int index) {
     if (index >= movies.size()) {
-      return ResponseEntity.badRequest().build();
+      return ResponseEntity.notFound().build();
     }
 
     Movie movie = movies.get(index);
-
-    if (movie == null) {
-      return ResponseEntity.notFound().build();
-    }
 
     return ResponseEntity.ok(movie);
   }
 
   @PostMapping("/movies")
-  public ResponseEntity<Movie> addMovie(@RequestBody Movie movie) {
-    if (movie.getTitle() == null) {
-      return ResponseEntity.badRequest().build();
+  public ResponseEntity<?> addMovie(@RequestBody Movie movie) {
+    if (movie.getTitle() == null || movie.getDirector() == null) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("movie title empty");
     }
 
     movies.add(movie);
@@ -44,5 +42,12 @@ public class MovieController {
     return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest()
             .path("/{index}")
             .buildAndExpand(movies.size() - 1).toUri()).body(movie);
+  }
+
+  @GetMapping("/movies/search")
+  public ResponseEntity<List<Movie>> searchByTitle(@RequestParam String title) {
+    List<Movie> movieList = movies.stream().filter(m -> m.getTitle().toLowerCase().contains(title.toLowerCase())).toList();
+
+    return ResponseEntity.ok(movieList);
   }
 }
